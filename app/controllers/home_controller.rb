8c79bdbfa226606a27c713f26e763a22673ca0e0
HirataTomoya <ht327champ@gmail.com>
2021-02-15 00:35:57 +0900

    [update]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     @user = User.find_by(password: params[:password])
     if @user
       session[:user_id] = @user.user_id
-      redirect_to("/home/top")
+      redirect_to("/home/menu1")
     else
       redirect_to("/home/loginform")
       flash[:notice] = "パスワードが間違っています"
@@ -18,7 +18,7 @@ class HomeController < ApplicationController
     redirect_to("/home/loginform")
   end
 
-  def top
+  def menu1
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   get 'test/problem'
   get '/' => "home#loginform"
-  get 'home/top' => 'home#top'
-  get 'home/top' => "home#top"
+  get 'home/menu1' => 'home#menu1'
   get 'home/login' => 'home#login'
   get 'home/logout' => 'home#logout'
   get 'home/leveltop/:id' => 'home#leveltop'
 
-  get 'test/score/1' => 'test#score'
-  get 'test/problem/1' => 'test#problem'
-  get 'test/answer/1' => 'test#answer'
+  get 'test/problem/:level/:id' => 'test#problem'
+  get 'test/answer/:level/:id' => 'test#answer'
   post 'home/login' => 'home#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
