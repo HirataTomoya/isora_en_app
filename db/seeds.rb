0828a36c1e3acbaaae39be94ccff7f4f13d66c73
HirataTomoya <ht327champ@gmail.com>
2021-02-22 13:14:15 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
-CSV.foreach('db/result_seed.csv') do |row|
-  Result.create(:user_id => row[0],
-                    :level_id => row[1],
-                    :word_id => row[2],
-                    :practice_id => row[3])
+# CSV.foreach('db/result_seed.csv') do |row|
+#   Result.create(:user_id => row[0],
+#                     :level_id => row[1],
+#                     :word_id => row[2],
+#                     :practice_id => row[3])
+# end
+
+CSV.foreach('db/word_seed.csv') do |row|
+  Word.create(:word_id => row[0],
+                    :word_en => row[1],
+                    :word_jp => row[2],
+                    :level_id => row[3],
+                    :category_id => row[4],
+                    :practice_id => row[5])
+end
+
+CSV.foreach('db/user_seed.csv') do |row|
+  User.create(:user_id => row[0],
+                    :password => row[1])
 end
